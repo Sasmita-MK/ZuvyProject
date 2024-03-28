af73988292a1e0387a4c65889dcade1bd4bca8a5
Sasmita-MK <124858997+Sasmita-MK@users.noreply.github.com>
2024-03-28 10:15:11 +0530

    First Commit

---
+++ Zuvy Projects/Vending Machine/src/App.java
@@ -0,0 +1,7 @@
+import coin.Coin;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int value = Coin.TEN.getValue();
+        System.out.print(value);
+    }
+}
